04c3565da852c01487f667cdb940d541fd64a676
Kirill Sh <kirill-sh2007@yandex.ru>
2020-10-22 15:16:15 +0300

    first version

---
+++ w_split_txt.py
@@ -0,0 +1,23 @@
+
+from tkinter import *
+
+root = Tk()
+
+ent = Entry(width=20)
+but = Button(text="Преобразовать")
+lab = Label(bg='black', fg='white', width=20)
+
+
+def str_to_sort_list(event):
+    s = ent.get()
+    s = s.split()
+    s.sort()
+    lab['text'] = ' '.join(s)
+
+
+but.bind('<Button-1>', str_to_sort_list)
+
+ent.pack()
+but.pack()
+lab.pack()
+root.mainloop()
